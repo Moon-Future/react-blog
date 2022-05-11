ad7d2abecfb558f626966bc300b23995792b90f1
liangchen25 <liangchen25@iflytek.com>
2022-05-11 14:30:46 +0800

    添加备案号

--- blog/components/Footer.js
+++ blog/components/Footer.js
@@ -3,6 +3,9 @@ import '../static/style/components/footer.less'
 const Footer = () => (
   <div className="footer-box">
     <div>人家也是有底线的啦~</div>
+    <div>
+      备案号：<a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备17145260号-2</a>
+    </div>
     {/* <div>
       <a href="//cl8023.com">cl8023.com</a>、
       <a href="//vue.cl8023.com" target="_blank">
